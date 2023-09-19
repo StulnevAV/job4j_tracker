dad9ca498142fb2d91c1148a7392306bb4f0e2b4
StulnevAV <StulnevAV@yandex.ru>
2023-09-19 21:59:51 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error error1 = new Error(true, 21313, "Ошибка 1");
+        error1.printInfo();
+        Error error2 = new Error(false, 32532, "Ошибка 2");
+        error2.printInfo();
+        Error error3 = new Error(true, 123344, "Ошибка 3");
+        error3.printInfo();
+    }
+}
