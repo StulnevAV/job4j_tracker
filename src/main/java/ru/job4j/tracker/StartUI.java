e93cfa958fb649a681e8121548a864a82557b8ad
StulnevAV <StulnevAV@yandex.ru>
2023-09-27 22:02:50 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
+
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String dateTime = item.getCreated().format(formatter);
-        System.out.println("Текущие дата и время: " + dateTime);
+        Item item = new Item("заявка", 21);
+        System.out.println(item);
     }
 }
