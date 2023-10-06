b5dc8e1060e95d307049943d83089268f1a82cf7
StulnevAV <StulnevAV@yandex.ru>
2023-10-06 21:03:26 +0300

    12.0. Написать тесты на StartUI. [#785 #450441]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -41,7 +41,7 @@ class StartUITest {
     void whenDeleteItem() {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        Item item = tracker.add(new Item("Deleted item")); /* Добавляется в tracker новая заявка */
+        Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
                 new String[]{"0", String.valueOf(item.getId()), "1"}
         );
